f83625e7f60f4c8a3be3e7785373b15d9db1c97a
Siddharth Singh <siddharths067@gmail.com>
2019-05-18 18:38:50 +0530

    updated CRUD and name form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Person extends React.Component{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor(props){
+        super(props)
+        this.state = {
+            name : this.props.name,
+            age : this.props.age
+        }
+        
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(){
+        alert("you clicked on " + this.state.name)
+    }
+    render(){
+        return (
+            <div className="person_holder" key={this.state.name} onClick ={this.handleClick}>
+                <h1>{this.state.name}</h1>
+                <h2>is {this.props.age} years old</h2>
+            </div>
+        )
+    }
+}
+
+
+class PersonList extends React.Component{
+
+    render(){
+        return (
+            <div className="person_list">
+                {
+                    Array.from(Array(this.props.len).keys()).map(
+                        i => <Person name = {this.props.names[i]} age = {this.props.ages[i]}/>
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+
+class NameForm extends React.Component{
+    constructor(props){
+        super(props)
+        this.nameList = []
+        this.ageList = []
+    }
+
+    updatePersonList() {
+        return () => {
+            this.nameList.push(document.getElementById("perName").value);
+            this.ageList.push(document.getElementById("perAge").value);
+            
+            document.getElementById("perName").value = ""
+            document.getElementById("perAge").value = ""
+            
+            ReactDOM.render(<PersonList names={this.nameList} ages={this.ageList} len={this.nameList.length} />, document.getElementById("listView"));
+        };
+    }
+
+    render(){
+        return (
+                <div id="crudApp">
+                Name : <input id="perName" type = "text"></input>
+                Age : <input id="perAge" type = "text"></input> <br></br>
+                <button onClick= {this.updatePersonList()}> Submit Person </button>
+                <div id="listView"></div>
+                </div>
+            )
+    }
+}
+
+
+ReactDOM.render(
+    <NameForm />
+    ,document.getElementById('root')
+)
+
+
+
